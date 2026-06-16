b7db636b8d2910cb10318d627623c11c9e741375
Karthikeyan V <133189732+Karthicr7@users.noreply.github.com>
2026-06-16 14:58:41 +0530

    Time: 27 ms (11.43%), Space: 12.3 MB (60.95%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.py
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.py
@@ -5,6 +5,8 @@ class Solution(object):
         :type weights: List[int]
         :rtype: str
         """
+        letters = {25: 'a', 24: 'b', 23: 'c', 22: 'd', 21: 'e', 20: 'f', 19: 'g', 18: 'h', 17: 'i', 16: 'j', 15: 'k', 14: 'l', 13: 'm', 12: 'n', 11: 'o', 10: 'p', 9: 'q', 8: 'r', 7: 's', 6: 't', 5: 'u', 4: 'v', 3: 'w', 2: 'x', 1: 'y', 0: 'z'}
+
         s=""
         for i in words:
             sum=0
@@ -12,6 +14,5 @@ class Solution(object):
                 position = ord(j.lower()) - ord('a') 
                 sum+=weights[position]
             p=sum%26
-            letter = chr(ord('z') - p)
-            s+=letter
+            s+=letters[p]
         return s
